e1c8039a57506ce0efd1dab6cc8f482301db2b06
Chris Jones <cjones@gmail.com>
2013-09-13 12:07:21 -0700

    attempt to make lyrics module work in more situations

--- madcow/modules/lyrics.py
+++ madcow/modules/lyrics.py
@@ -1,6 +1,8 @@
 """Get song lyrics from lyricwiki"""
 
-from urlparse import urljoin
+from urlparse import urlparse, urljoin, urlunparse
+from urllib import urlencode
+from cgi import parse_qsl
 import re
 from madcow.util import Module, strip_html
 from madcow.util.http import getsoup
@@ -31,26 +33,57 @@ class Main(Module):
         self.google = Google()
 
     def response(self, nick, args, kwargs):
-        try:
-            url = self.google.lucky(u'site:songmeanings.net ' + args[0])
-        except NonRedirectResponse:
-            self.log.warn('no url for query {0!r} found from google lucky'.format(args[0]))
-            return u'{nick}: {error}'.format(error=self.error, **kwargs)
+        url = urlunparse(('https', 'www.google.com', 'search', '',
+            urlencode({'num': '100', 'safe': 'off', 'hl': 'en', 'q': 'site:songmeanings.net ' + args[0]}), ''))
+        soup = getsoup(url)
+        new = None
+        for h3 in soup.findAll('h3', attrs={'class': 'r'}):
+            uri = urlparse(h3.a['href'])
+            if uri.path == '/url':
+                url = dict(parse_qsl(uri.query))['q']
+                uri = urlparse(url)
+                if re.search('/songs/view/\d+', uri.path) is not None:
+                    new = urlunparse(uri._replace(query='', fragment=''))
+                    break
+                elif re.search('/profiles/(submissions|interaction)/\d+/comments', uri.path) is not None:
+                    soup = getsoup(url)
+                    for a in soup.find('a', title='Direct link to comment'):
+                        new = urlunparse(urlparse(a.parent['href'])._replace(fragment='', query=''))
+                        break
+                if new:
+                    break
+        if new:
+            url = new
+            #try:
+            #    url = self.google.lucky(u'site:songmeanings.net ' + args[0])
+            #except NonRedirectResponse:
+            #    self.log.warn('no url for query {0!r} found from google lucky'.format(args[0]))
+            #    return u'{nick}: {error}'.format(error=self.error, **kwargs)
 
-        try:
-            soup = getsoup(url)
+            ## XXX sometimes the only google link is to a comment on the thread for some reason beyond my ken
+            #uri = urlparse(url)
+            #if re.search('/profiles/interaction/\d+/comments', uri.path) is not None:
+            #    soup = getsoup(url)
+            #    for a in soup.find('a', title='Direct link to comment'):
+            #        url = urlunparse(urlparse(a.parent['href'])._replace(fragment='', query=''))
+            #        break
+
             try:
-                title = strip_html(soup.find('a', 'pw_title').renderContents()).strip()
+                soup = getsoup(url)
+                try:
+                    title = re.sub('\s+Lyrics\s+\|\s+SongMeanings.*$', '', soup.title.renderContents())
+                    #title = strip_html(soup.find('a', 'pw_title').renderContents()).strip()
+                except StandardError:
+                    title = 'Unknown artist/song, check parsing code!'
+                    #ipython()
+                text = soup.find('div', id='textblock')
             except StandardError:
-                title = 'Unknown artist/song, check parsing code!'
-            text = soup.find('div', id='textblock')
-        except StandardError:
-            self.log.warn('unable to find textblock from url {0!r} (query: {1!r})'.format(url, args[0]))
-            return u'{nick}: {error}'.format(error=self.error, **kwargs)
+                self.log.warn('unable to find textblock from url {0!r} (query: {1!r})'.format(url, args[0]))
+                return u'{nick}: {error}'.format(error=self.error, **kwargs)
 
-        try:
-            lyrics = decode(text.renderContents(), 'utf-8')
-            return u'\n'.join(['[{}]'.format(title)] + filter(None, [line.strip() for line in strip_html(lyrics).splitlines()]))
-        except StandardError:
-            self.log.exception('error parsing lyrics for query: {0!r}'.format(args[0]))
-            return u'{nick}: {error}'.format(error=self.error, **kwargs)
+            try:
+                lyrics = decode(text.renderContents(), 'utf-8')
+                return u'\n'.join(['[{}]'.format(title)] + filter(None, [line.strip() for line in strip_html(lyrics).splitlines()]))
+            except StandardError:
+                self.log.exception('error parsing lyrics for query: {0!r}'.format(args[0]))
+                return u'{nick}: {error}'.format(error=self.error, **kwargs)
